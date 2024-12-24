e744cf6c2b76530bec4a22b512097659be04cf32
XuWuLingYu <2623323974@qq.com>
2024-12-24 12:49:09 +0800

    delete build

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/qianzezhong/Documents/VSCode_Projects/lidar_new/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
--- build/livox_ros_driver/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/qianzezhong/Documents/VSCode_Projects/lidar_new/devel/include".split(';') if "/home/qianzezhong/Documents/VSCode_Projects/lidar_new/devel/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;rospy;std_msgs;message_runtime;pcl_ros".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "livox_ros_driver"
-PROJECT_SPACE_DIR = "/home/qianzezhong/Documents/VSCode_Projects/lidar_new/devel"
-PROJECT_VERSION = "2.0.0"
